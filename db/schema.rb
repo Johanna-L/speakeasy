e79055abc95ff4d3ffc8758bf098860c316daa31
Johanna Laversin <johanna.laversin@gmail.com>
2019-11-19 15:46:40 +0100

    dashboard and updated navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_165939) do
+ActiveRecord::Schema.define(version: 2019_11_19_134929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_11_18_165939) do
     t.text "social_media"
     t.string "company_name"
     t.text "company_info"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,100 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Clear seed
+puts "Destroying existing users"
+User.destroy_all
+
+# Speakers
+puts "Initializing speakers"
+
+barack = User.new({
+  first_name: "Barack",
+  last_name: "Obama",
+  email: "barack.obama@gmail.com",
+  password: "barack",
+  password_confirmation: "barack",
+  photo: "barack.jpg",
+  social_media: "https://twitter.com/barackobama",
+  background: "Ancien président des Etats-Unis"
+})
+
+barack.save!
+
+laure = User.new({
+  first_name: "Laure",
+  last_name: "Manaudou",
+  email: "laure.manaudou@gmail.com",
+  password: "manaudou",
+  password_confirmation: "manaudou",
+  photo: "laure.jpg",
+  social_media: "https://twitter.com/manaudou?lang=fr",
+  background: "Championne olympique de natation"
+})
+
+laure.save!
+
+nabila = User.new({
+  first_name: "Nabilla",
+  last_name: "Benattia-Vergara",
+  email: "nabilla@gmail.com",
+  password: "nabilla",
+  password_confirmation: "nabilla",
+  photo: "nabila.jpg",
+  social_media: "https://twitter.com/Nabilla",
+  background: "Tu me connais pas? Allô quoi!"
+})
+
+nabila.save!
+
+balkany = User.new({
+  first_name: "Patrick",
+  last_name: "Balkany",
+  email: "patrick.balkany@gmail.com",
+  password: "balkany",
+  password_confirmation: "balkany",
+  photo: "balkany.jpg",
+  social_media: "https://twitter.com/patrick_balkany",
+  background: "Expert en fraude fiscale"
+})
+
+balkany.save!
+
+emma = User.new({
+  first_name: "Emma",
+  last_name: "Watson",
+  email: "emma.watson@gmail.com",
+  password: "watson",
+  password_confirmation: "watson",
+  photo: "emma.jpg",
+  social_media: "https://twitter.com/emmawatson",
+  background: "Actrice, ambassadrice pour le droit des femmes, engagée pour le développement durable."
+})
+
+emma.save!
+
+patrick = User.new({
+  first_name: "Patrick",
+  last_name: "Sébastien",
+  email: "patrick.sebastien@gmail.com",
+  password: "patrick",
+  password_confirmation: "patrick",
+  photo: "patrick.jpg",
+  social_media: "https://twitter.com/PatSebastien",
+  background: "Chanteur, présentateur télé, met l'ambiance dans vos soirées."
+})
+
+patrick.save!
+
+# Organizers
+puts "Initializing organizers"
+
+fred = User.new({
+  first_name: "Fred",
+  last_name: "Durand",
+  email: "fred.durand@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+  photo: "fred.png",
+  company_name: "Best Conferences",
+  company_info: "Des conférences exceptionnelles, avec des intervenants de qualité."
+})
+
+fred.save!
