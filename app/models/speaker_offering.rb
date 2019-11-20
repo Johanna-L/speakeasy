01169a3e1ff352d9856e31ae671764b667b6f7df
Johanna Laversin <johanna.laversin@gmail.com>
2019-11-20 12:59:27 +0100

    Dashboard màj, navbar màj avec avatar, titre, navigation modifiée

--- app/models/speaker_offering.rb
+++ app/models/speaker_offering.rb
@@ -1,5 +1,5 @@
 class SpeakerOffering < ApplicationRecord
+  validates :title, presence: true
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  has_many :bookings, dependent: :destroy
+  has_many :speaker_offerings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   has_many :bookings
-
+  def offer_bookings
+    Booking.where(speaker_offering: speaker_offerings)
+  end
 end
-
