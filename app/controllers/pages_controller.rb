a592ff4db4a13ceb95ada141d493a4768d671e24
Johanna Laversin <johanna.laversin@gmail.com>
2019-11-21 23:30:26 +0100

    réparation des liens de la home vers les show correspondantes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,17 @@ require 'time'
 
 class PagesController < ApplicationController
   def home
+    @obama_user = User.find_by(last_name: 'Obama')
+    @obama_offer_id = SpeakerOffering.find_by(user: @obama_user)
+
+    @watson_user = User.find_by(last_name: 'Watson')
+    @watson_offer_id = SpeakerOffering.find_by(user: @watson_user)
+
+    @sebastien_user = User.find_by(last_name: 'Sébastien')
+    @sebastien_offer_id = SpeakerOffering.find_by(user: @sebastien_user)
+
+    @benattia_user = User.find_by(last_name: 'Benattia')
+    @benattia_offer_id = SpeakerOffering.find_by(user: @benattia_user)
   end
 
   def dashboard
